034b1d80df634487e351a1d7552679da0322d8da
schaeferC <claudia.schaef@gmx.net>
2011-12-15 01:12:49 +0100

    das projekt ist da

---
+++ BeuthOrg/src/de/bht/BeuthOrg/BeuthOrgActivity.java
@@ -0,0 +1,31 @@
+package de.bht.BeuthOrg;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class BeuthOrgActivity extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+		   
+	private Button helloButton;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+                
+        helloButton = (Button)findViewById(R.id.hello_button);
+        helloButton.setOnClickListener(this);
+    }
+
+	@Override
+	public void onClick(View v) {
+		if (v == helloButton){
+			 startActivity(new Intent("de.bht.BeuthOrg.Menu"));
+		}	
+	}
+}
---
+++ BeuthOrg/src/de/bht/BeuthOrg/Menu.java
@@ -0,0 +1,32 @@
+package de.bht.BeuthOrg;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Menu extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+    }
+
+	@Override
+	public void onClick(View v) {
+	
+		
+		
+	}
+}
