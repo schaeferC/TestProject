e5951b4c482f07251070e3e2afc8ea8de9a7c506
schaeferC <claudia.schaef@gmx.net>
2011-12-18 13:41:32 +0100

    Endlich die FakeApi zum größten Teil durch.

---
+++ BeuthOrg/fakeapi/writedata/CreateTestData.java
@@ -0,0 +1,21 @@
+package writedata;
+
+
+import org.json.JSONException;
+
+/**
+ * @author Claudia Schafer
+ *
+ */
+class CreateTestData {
+
+	/**
+	 * @param args
+	 * @throws JSONException 
+	 */
+	public static void main(String[] args) throws JSONException {
+		writeData.selectDialog();
+		
+	}
+	
+}
