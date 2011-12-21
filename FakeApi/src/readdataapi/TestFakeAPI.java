8d9f27fea477533a831970380332b965d8359c89
schaeferC <claudia.schaef@gmx.net>
2011-12-21 19:01:00 +0100

    extraFakeAPI

---
+++ FakeApi/src/readdataapi/TestFakeAPI.java
@@ -0,0 +1,23 @@
+package readdataapi;
+
+
+/**
+ * @author Claudia Schaefer
+ *
+ */
+public class TestFakeAPI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		System.out.println(ReadData.LogIn("000041", "1234").toString());
+		System.out.println(ReadData.getAllEvents().toString());
+		System.out.println(ReadData.getAllLehrkraftnews().toString());
+		System.out.println(ReadData.getProfDataByProfname("Grude").toString());
+		System.out.println(ReadData.getModulDescriptionByStudOrd("1", "MME1").toString());
+//		System.out.println(ReadData.getStudienDoku().toString());
+		
+	}
+
+}
---
+++ FakeApi/src/writetestdata/CreateTestData.java
@@ -0,0 +1,20 @@
+package writetestdata;
+
+import org.json.JSONException;
+
+/**
+ * @author Claudia Schafer
+ *
+ */
+class CreateTestData {
+
+	/**
+	 * @param args
+	 * @throws JSONException 
+	 */
+	public static void main(String[] args) throws JSONException {
+		writeData.selectDialog();
+		
+	}
+	
+}
