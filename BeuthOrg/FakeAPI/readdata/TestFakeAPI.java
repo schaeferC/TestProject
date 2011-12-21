aa2c7474208fd9f3c9e9295d5b1b9bb4bbfe2db3
schaeferC <claudia.schaef@gmx.net>
2011-12-21 18:16:31 +0100

    Aenderungen FakaData

--- BeuthOrg/FakeAPI/readdata/TestFakeAPI.java
+++ BeuthOrg/FakeAPI/readdata/TestFakeAPI.java
@@ -12,7 +12,7 @@ public class TestFakeAPI {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(ReadData.LogIn("770041", "1234").toString());
+		System.out.println(ReadData.LogIn("000041", "1234").toString());
 		System.out.println(ReadData.getAllEvents().toString());
 		System.out.println(ReadData.getAllLehrkraftnews().toString());
 		System.out.println(ReadData.getProfDataByProfname("Grude").toString());
