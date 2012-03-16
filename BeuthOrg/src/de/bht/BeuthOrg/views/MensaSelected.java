721a5aa8c9bd633fefa5fdf400e891e7038885e0
schaeferC <claudia.schaef@gmx.net>
2012-03-16 20:48:49 +0100

    mit mensa essen anbindung

--- BeuthOrg/src/de/bht/BeuthOrg/views/MensaSelected.java
+++ BeuthOrg/src/de/bht/BeuthOrg/views/MensaSelected.java
@@ -1,9 +1,14 @@
 package de.bht.BeuthOrg.views;
 
+import javax.xml.datatype.Duration;
+
 import de.bht.BeuthOrg.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
@@ -12,21 +17,53 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MensaSelected extends Activity {
-	ImageView mensaWabe;
+
+	private ImageView mensaWabe;
+	private Button food;
+	private OnClickListener onClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			if (v == food) {
+				Log.w("click", "food clicked");
+
+				startActivity(new Intent(v.getContext(), MensaFood.class));
+			}
+
+		}
+	};;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mensamenu);
+		
 		mensaWabe = (ImageView) findViewById(R.id.imageView1);
-		Animation a = AnimationUtils.loadAnimation(this, R.anim.menubuttonsscale);
-		mensaWabe.startAnimation(a);
+		food = (Button) findViewById(R.id.foodb);
+		food.setOnClickListener(onClick);
+		
+		Animation aScale = AnimationUtils.loadAnimation(this,
+				R.anim.menubuttonsscale);
+		Animation aAlpha = AnimationUtils.loadAnimation(this,
+				R.anim.menubuttonsalpha);
+		Animation aRotate = AnimationUtils.loadAnimation(this,
+				R.anim.menubuttonsrotate);
+		
+		mensaWabe.startAnimation(aScale);
+		food.startAnimation(aAlpha);
+		
+//		
+//
+//		food = (Button) findViewById(R.id.foodb);
+//		food.bringToFront();
+		
 	}
-
-
 
 }
