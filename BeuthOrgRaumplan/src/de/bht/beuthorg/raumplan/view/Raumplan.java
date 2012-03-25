6095f3096c2636f3e02c8fa8f88a0153fc57b548
schaeferC <claudia.schaef@gmx.net>
2012-03-25 05:50:39 +0200

    raumplan modul

---
+++ BeuthOrgRaumplan/src/de/bht/beuthorg/raumplan/view/Raumplan.java
@@ -0,0 +1,16 @@
+package de.bht.beuthorg.raumplan.view;
+
+import de.bht.beuthorg.raumplan.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Raumplan extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.raumplan);
+	}
+
+}
