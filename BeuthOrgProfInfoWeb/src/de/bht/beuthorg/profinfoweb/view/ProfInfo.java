5bfe889a1abcf5bf2294d4cdadb4a604f95f368d
schaeferC <claudia.schaef@gmx.net>
2012-03-25 23:48:02 +0200

    mit Kommentaren

--- BeuthOrgProfInfoWeb/src/de/bht/beuthorg/profinfoweb/view/ProfInfo.java
+++ BeuthOrgProfInfoWeb/src/de/bht/beuthorg/profinfoweb/view/ProfInfo.java
@@ -9,22 +9,42 @@ import android.webkit.WebView;
 import de.bht.beuthorg.datahandler.DataHandler;
 import de.bht.beuthorg.profinfoweb.R;
 
+/**
+ * Activity für die Darstellung der Website des Profs
+ * 
+ * @author Claudia
+ * 
+ */
 public class ProfInfo extends Activity {
-	
-	public static final int PROFINFO_REQUEST_CODE = 321900;
 
+	/**
+	 * Constante mit der die Activity aufgerufen wird
+	 */
+	public static final int PROFINFO_REQUEST_CODE = 321900;
+	/**
+	 * Result, dass die Activity zurückgibt nach erfolgreichem Abschluss
+	 */
 	public static final int PROFINFO_SUCCESS_CODE = 257;
-	
+
+	/**
+	 * Objekt welches die Website anzeigt
+	 */
 	private WebView webview;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// zugrundeliegendes Layout
 		setContentView(de.bht.beuthorg.profinfoweb.R.layout.profinfo);
+
+		// Zuweisung aus dem zugrundeliegendem Layout
 		webview = (WebView) findViewById(R.id.webview);
 		webview.getSettings().setJavaScriptEnabled(true);
+		// get benötigte Inhalte für die URL
 		String intentExtra = getIntent().getStringExtra("modul");
-		Log.w("Dedug", "ProfInfo "+intentExtra.substring(intentExtra.lastIndexOf("\n")+1));
-		String website = DataHandler.getProfWebsite(intentExtra.substring(intentExtra.lastIndexOf("\n")+1));
+		String website = DataHandler.getProfWebsite(intentExtra
+				.substring(intentExtra.lastIndexOf("\n") + 1));
+		// Website aufrufen
 		webview.loadUrl(website);
 	}
 
@@ -36,6 +56,10 @@ public class ProfInfo extends Activity {
 		return true;
 	}
 
+	/**
+	 * Beendet die aktuelle Activity und gibt der aufrufenden Activity den
+	 * SuccessCode zurück
+	 */
 	protected void returnToCallingActivity() {
 		Intent intent = new Intent();
 		setResult(ProfInfo.PROFINFO_SUCCESS_CODE, intent);
