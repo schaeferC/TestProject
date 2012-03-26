b47c49afb9bff2cd019ca75d732b75448f4d6712
hyperrr <bloody-g@gmx.de>
2012-03-26 02:37:02 +0200

    kommentare hinzugefügt

--- BeuthOrgRaumplan/src/de/bht/beuthorg/raumplan/view/Raumplan.java
+++ BeuthOrgRaumplan/src/de/bht/beuthorg/raumplan/view/Raumplan.java
@@ -9,16 +9,27 @@ import android.view.KeyEvent;
 
 public class Raumplan extends Activity {
 
+	/**
+	 * Codes zur Identifizierung bzw. zum Aufruf der Activity 
+	 */
 	public static final int RAUMPLAN_REQUEST_CODE = 523399;
 
 	public static final int RAUMPLAN_SUCCESS_CODE = 8852;
 
+	/** 
+	 * betroffenes Layout wird mit Hilfe von setContentView-Methode mit Activity verknüpft, 
+	 * um auf die XML-Komponenten zugreifen zu können.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.raumplan);
 	}
 	
+	/** 
+	 * Methode sorgt dafür, dass beim Drücken der BACK-Taste des Smartphones
+	 * die jeweilig vorherige Activity aufgerufen wird. 
+	 */
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -27,6 +38,10 @@ public class Raumplan extends Activity {
 		return true;
 	}
 
+	/**
+	 * Beendet die aktuelle Activity und gibt der aufrufenden Activity den
+	 * SuccessCode zurück
+	 */
 	protected void returnToCallingActivity() {
 		Intent intent = new Intent();
 		setResult(Raumplan.RAUMPLAN_SUCCESS_CODE, intent);
